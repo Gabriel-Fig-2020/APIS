8d6afcec6774a7922a96fca2dce53f9f69566c81
gabriel-fig-2020 <gabrielfgsantos95@gmail.com>
2021-04-25 17:37:12 -0300

    final crud drf

--- school/grades/views.py
+++ school/grades/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
 
 # Create your views here.
-def grades(request):
-    if request.method == 'GET':
-        return HttpResponse('my grades')
+class GradeViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+# def grades(request):
+#     if request.method == 'GET':
+#         students = Student.objects.first()
+#         serializer = StudentSerializer(students)
+#         return JsonResponse(serializer.data)
--- school/school/urls.py
+++ school/school/urls.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
-from django.urls import path
-from grades.views import grades
+from django.urls import path, include
+from grades.views import GradeViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
+
+router = DefaultRouter()
+router.register(r'grades', GradeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('grades/', grades, name='grades'),
+    path('', include(router.urls)),
+    # path('grades/', grades, name='grades'),
 ]
